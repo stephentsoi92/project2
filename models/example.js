c10fdee291ebabf738563519a2f19faf96332783
RicardoJavierFernandez <ricky@Ricardos-MacBook-Pro.local>
2019-09-25 20:28:14 -0400

    Changed the database name

--- models/example.js
+++ models/example.js
@@ -1,7 +1,7 @@
 module.exports = function(sequelize, DataTypes) {
-  var Example = sequelize.define("Example", {
+  var Employees = sequelize.define("Employees", {
     text: DataTypes.STRING,
     description: DataTypes.TEXT
   });
-  return Example;
+  return Employees;
 };
