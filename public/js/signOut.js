e919d2d74ab221254b1e4009f609b10210cc1039
andreia <andreia@andreias-mbp.attlocal.net>
2019-10-01 12:17:38 -0400

    signout works. signout links will need to be updated once site goes live.

---
+++ public/js/signOut.js
@@ -0,0 +1,12 @@
+function signOut(){
+	
+	window.location.href = "file:///Users/andreia/coding_bootcamp/projects/project2/project2/public/index.html";
+		
+		};
+
+		function remain(){
+	
+			window.location.href = "#";
+				
+				};
+		
