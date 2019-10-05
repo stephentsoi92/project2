b43fd49b1fc98b92d01edbf8d2733849cddc63cf
andreia <andreia@andreias-mbp.attlocal.net>
2019-10-04 23:51:58 -0400

    removing alert on login

--- public/js/login.js
+++ public/js/login.js
@@ -2,7 +2,6 @@ function validate(){
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
     if ( username == "admin" && password == "password"){
-    alert ("Login successfully");
     window.location.href = "../calendar.html";
 
       }
